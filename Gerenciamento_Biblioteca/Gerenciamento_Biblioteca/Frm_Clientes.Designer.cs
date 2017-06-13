0dcd56bc237aa01a190fa9865db633a95aaaa0c3
lucasdaconceicao <lucasbuckerdvni@gmail.com>
2017-06-13 12:04:58 -0300

    frm_clientes

---
+++ Gerenciamento_Biblioteca/Gerenciamento_Biblioteca/Frm_Clientes.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Gerenciamento_Biblioteca
+{
+    partial class Frm_Clientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Clientes));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbtnAlterarExcluir = new System.Windows.Forms.ToolStripButton();
+            this.tsbtnCadastrar = new System.Windows.Forms.ToolStripButton();
+            this.tsbtnConsultar = new System.Windows.Forms.ToolStripButton();
+            this.tsbtnVoltar = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.tsbtnSair = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbtnAlterarExcluir,
+            this.tsbtnCadastrar,
+            this.tsbtnConsultar,
+            this.tsbtnVoltar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(3, 3, 1, 0);
+            this.toolStrip1.Size = new System.Drawing.Size(634, 58);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbtnAlterarExcluir
+            // 
+            this.tsbtnAlterarExcluir.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnAlterarExcluir.Image")));
+            this.tsbtnAlterarExcluir.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsbtnAlterarExcluir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnAlterarExcluir.Name = "tsbtnAlterarExcluir";
+            this.tsbtnAlterarExcluir.Size = new System.Drawing.Size(133, 52);
+            this.tsbtnAlterarExcluir.Text = "Alterar/Excluir";
+            // 
+            // tsbtnCadastrar
+            // 
+            this.tsbtnCadastrar.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnCadastrar.Image")));
+            this.tsbtnCadastrar.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsbtnCadastrar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnCadastrar.Name = "tsbtnCadastrar";
+            this.tsbtnCadastrar.Size = new System.Drawing.Size(109, 52);
+            this.tsbtnCadastrar.Text = "Cadastrar";
+            // 
+            // tsbtnConsultar
+            // 
+            this.tsbtnConsultar.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnConsultar.Image")));
+            this.tsbtnConsultar.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsbtnConsultar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnConsultar.Name = "tsbtnConsultar";
+            this.tsbtnConsultar.Size = new System.Drawing.Size(110, 52);
+            this.tsbtnConsultar.Text = "Consultar";
+            // 
+            // tsbtnVoltar
+            // 
+            this.tsbtnVoltar.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnVoltar.Image")));
+            this.tsbtnVoltar.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsbtnVoltar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnVoltar.Name = "tsbtnVoltar";
+            this.tsbtnVoltar.Size = new System.Drawing.Size(90, 52);
+            this.tsbtnVoltar.Text = "Voltar";
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip2.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbtnSair});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 406);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(634, 55);
+            this.toolStrip2.TabIndex = 2;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // tsbtnSair
+            // 
+            this.tsbtnSair.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbtnSair.Image = ((System.Drawing.Image)(resources.GetObject("tsbtnSair.Image")));
+            this.tsbtnSair.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsbtnSair.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbtnSair.Name = "tsbtnSair";
+            this.tsbtnSair.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.tsbtnSair.Size = new System.Drawing.Size(57, 52);
+            this.tsbtnSair.Text = "toolStripButton5";
+            // 
+            // Frm_Clientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 461);
+            this.Controls.Add(this.toolStrip2);
+            this.Controls.Add(this.toolStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.IsMdiContainer = true;
+            this.MaximizeBox = false;
+            this.Name = "Frm_Clientes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clientes";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbtnAlterarExcluir;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripButton tsbtnSair;
+        public System.Windows.Forms.ToolStripButton tsbtnVoltar;
+        private System.Windows.Forms.ToolStripButton tsbtnCadastrar;
+        private System.Windows.Forms.ToolStripButton tsbtnConsultar;
+    }
+}
