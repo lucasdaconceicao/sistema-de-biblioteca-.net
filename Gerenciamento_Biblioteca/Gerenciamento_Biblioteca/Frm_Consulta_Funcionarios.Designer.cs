fc6eabe1fcbb1c9d3457509d2e62dbeaceccbe24
lucasdaconceicao <lucasbuckerdvni@gmail.com>
2017-06-17 02:14:04 -0300

    script tabelas

---
+++ Gerenciamento_Biblioteca/Gerenciamento_Biblioteca/Frm_Consulta_Funcionarios.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Gerenciamento_Biblioteca
+{
+    partial class Frm_Consulta_Funcionarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Frm_Consulta_Funcionarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 400);
+            this.Name = "Frm_Consulta_Funcionarios";
+            this.Text = "Frm_Consulta_Funcionarios";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
--- Gerenciamento_Biblioteca/Gerenciamento_Biblioteca/Frm_Consulta_Livros.cs
+++ Gerenciamento_Biblioteca/Gerenciamento_Biblioteca/Frm_Consulta_Livros.cs
@@ -18,25 +18,73 @@ namespace Gerenciamento_Biblioteca
             InitializeComponent();
         }
 
-        private void recarregaGrid()
+        private void recarregarGrid()
         {
-            //Limpa os dados da grid
-            dgv_Livros.Rows.Clear();
-
             //conexao com o bd
             MySqlConnection conn = new MySqlConnection("Server=localhost;Port=3306;Database=BIBLIOTECA;Uid=lucas;Pwd=root;");
+            try
+            {
+                //verificando se o campo da busca nao esta vazio
+                if (String.IsNullOrEmpty(txtNome_Busca.Text))
+                {
+                    MessageBox.Show("O campo para busca nao pode ser vazio!");
+                    return;
+                }
+                
+                //Limpa os dados da grid
+                dgv_Livros.Rows.Clear();
 
-            //Criando um objeto Reader;
-            MySqlDataReader MyqlReader = null;
-            
-            string consulta = "SELECT * FROM LIVROS";
+                //abrindo a conexao com o bd
+                conn.Open();
 
+                //Criando um objeto Reader;
+                MySqlDataReader MysqlReader = null;
 
+                if (conn.State == ConnectionState.Open)
+                {
+                    //consulta no bd
+                    MySqlCommand comando = conn.CreateCommand();
+                    string consulta = "SELECT * FROM LIVROS WHERE NOME_LIVRO LIKE'%"+txtNome_Busca.Text+"%'" ;
+                    comando.CommandText = consulta;
+
+                    //retornando os dados da query
+                    MysqlReader = comando.ExecuteReader();
+
+                    //verificando se existe registro
+                    if (MysqlReader.HasRows == false)
+                    {
+                        MessageBox.Show("Nao existe registro!");
+                        txtNome_Busca.Clear();
+                        return;
+                    }
+
+                    //Percorrendo a consulta e adicionando os valores em cada linha
+                    while (MysqlReader.Read())
+                    {
+                        object[] valores = new object[MysqlReader.FieldCount];
+                        for (int i = 0; i < MysqlReader.FieldCount; i++)
+                        {
+                            valores[i] = MysqlReader.GetValue(i);
+                        }
+                        dgv_Livros.Rows.Add(valores);
+                    }
+                }
+                txtNome_Busca.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro:. " + ex.Message);
+            }
+            finally
+            {
+                //fechando a conexao com o banco de dados
+                conn.Close();
+            }
         }
 
         private void btnbusca_Click(object sender, EventArgs e)
         {
-
+            recarregarGrid();
         }
     }
 }
